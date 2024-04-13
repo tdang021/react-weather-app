9ead146485c900a007dc362a6820c3e4d71d6723
tdang021 <tdang021@users.noreply.github.com>
2024-04-12 23:47:45 -0400

    Added open source GitHub link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Search />
     </div>
   );
 }
---
+++ src/Coded.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Coded.css";
+
+export default function Coded() {
+  return (
+    <div className="codeSource">
+      <p>
+        Coded by Tiffany, open sourced on{" "}
+        <a
+          href="https://github.com/tdang021/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </div>
+  );
+}
